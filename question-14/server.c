990f424ee19da8e2e6be225a04e842a46b76c410
Rahul Anil Nayak <whorahulnayak@gmail.com>
2022-07-19 11:16:56 +0530

    question 14

---
+++ question-14/server.c
@@ -0,0 +1,59 @@
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<stdio.h>
+#include <string.h>
+
+#define max 100
+#define port 8080
+
+int main()
+{
+    int arr[10],n;
+    struct sockaddr_in serveraddr;
+    struct sockaddr_in clientaddr;
+    int sockfd = socket(AF_INET,SOCK_STREAM,0);
+    memset(&serveraddr,0,sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons(port);
+
+    bind(sockfd,(struct sockaddr *)&serveraddr,sizeof(serveraddr));
+    listen(sockfd,4);
+    while(1)
+    {
+        int clientLen = sizeof(clientaddr);
+        int newsockfd = accept(sockfd,(struct sockaddr *)&clientaddr,&clientLen);
+        printf("\nconnected to ip adddress: %s",inet_ntoa(clientaddr.sin_addr));
+        int pid = fork();
+        if(pid == 0)
+        {
+            while(1)
+            {
+                read(sockfd,&n,sizeof(n));
+                read(sockfd,&arr,10*sizeof(int));
+                for(int i=0;i<10;i++)
+                {
+                    for(int j=i+1;j<10;j++)
+                    {
+                        if(arr[i]>arr[j])
+                        {
+                            int temp = arr[i];
+                            arr[i] = arr[j];
+                            arr[j] = temp;
+                        }
+                    }
+                }
+                write(sockfd,&arr,10*sizeof(int));
+            }
+            exit(0);
+        }
+        else
+            close(newsockfd);
+    }
+
+    return 0;
+}
+
