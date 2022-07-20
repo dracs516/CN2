1a1351fa506955f1feb08e8c6b3a7916d7a6480c
Navaneeth Suresh <navaneeth22yt@gmail.com>
2022-07-20 19:50:59 +0530

    hopefully last changes

--- q17-file-content-udp/client.c
+++ q17-file-content-udp/client.c
@@ -7,39 +7,41 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-	
+
 #define PORT 8080
 #define MAXLINE 1024
-	
+
 // Driver code
-int main() {
+int main()
+{
 	int sockfd;
-	char filename[MAXLINE],ans[MAXLINE];
+	char filename[MAXLINE], ans[MAXLINE];
 	char *hello = "Hello from client";
-	struct sockaddr_in	 servaddr;
-	
+	struct sockaddr_in servaddr;
+
 	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	memset(&servaddr, 0, sizeof(servaddr));
-		
+
 	// Filling server information
 	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
 	servaddr.sin_addr.s_addr = INADDR_ANY;
-		
+
 	int n, len;
 	printf("\nEnter file name :");
-    scanf("%s",&filename);
-	sendto(sockfd, &filename, sizeof(filename),MSG_CONFIRM, (const struct sockaddr *) &servaddr,sizeof(servaddr));
+	scanf("%s", &filename);
+	sendto(sockfd, &filename, sizeof(filename), MSG_CONFIRM, (const struct sockaddr *)&servaddr, sizeof(servaddr));
 	printf("filename sent.\n");
-			
-	n = recvfrom(sockfd, &ans, MAXLINE,MSG_WAITALL, (struct sockaddr *) &servaddr,&len);
+
+	n = recvfrom(sockfd, &ans, MAXLINE, MSG_WAITALL, (struct sockaddr *)&servaddr, &len);
 	printf("Server : %s\n", ans);
-	
+
 	close(sockfd);
 	return 0;
 }
--- q17-file-content-udp/server.c
+++ q17-file-content-udp/server.c
@@ -7,72 +7,77 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
-	
-#define PORT	 8080
+
+#define PORT 8080
 #define MAXLINE 1024
-	
+
 // Driver code
-int main() {
+int main()
+{
 	int sockfd;
-	char filename[MAXLINE],ans[MAXLINE];
+	char filename[MAXLINE], ans[MAXLINE];
 	// char *hello = "Hello from server";
 	struct sockaddr_in servaddr, cliaddr;
-		
+
 	// Creating socket file descriptor
-	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+	{
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-		
+
 	memset(&servaddr, 0, sizeof(servaddr));
 	memset(&cliaddr, 0, sizeof(cliaddr));
-		
+
 	// Filling server information
 	servaddr.sin_family = AF_INET; // IPv4
 	servaddr.sin_addr.s_addr = INADDR_ANY;
 	servaddr.sin_port = htons(PORT);
-		
+
 	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,sizeof(servaddr)) < 0 )
+	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
 	{
 		perror("bind failed");
 		exit(EXIT_FAILURE);
 	}
-		
+
 	int len, n;
-	
-	len = sizeof(cliaddr); //len is value/result
-	
-	n = recvfrom(sockfd, &filename, MAXLINE,MSG_WAITALL, ( struct sockaddr *) &cliaddr,&len);
+
+	len = sizeof(cliaddr); // len is value/result
+
+	n = recvfrom(sockfd, &filename, MAXLINE, MSG_WAITALL, (struct sockaddr *)&cliaddr, &len);
 	printf("Client : %s\n", filename);
-                FILE* ptr;
-                char ch;
+	FILE *ptr;
+	char ch;
 
-                // Opening file in reading mode
-                ptr = fopen(filename, "r");
-            
-                if (NULL == ptr) {
-                    printf("file can't be opened \n");
-                }
-                else{
-                    printf("content of this file are \n");
-                    int k=0;
-                    do {
-                        ch = fgetc(ptr);
-                        printf("%c", ch);
-                        ans[k++] = ch;
-                
-                        // Checking if character is not EOF.
-                        // If it is EOF stop eading.
-                    } while (ch != EOF);
-                    ans[k] = '\0';
-                
-                    // Closing the file
-                    fclose(ptr);
-                }
+	// Opening file in reading mode
+	ptr = fopen(filename, "r");
 
-	sendto(sockfd, &ans, sizeof(ans),MSG_CONFIRM, (const struct sockaddr *) &cliaddr,len);
+	if (NULL == ptr)
+	{
+		printf("file can't be opened \n");
+	}
+	else
+	{
+		printf("content of this file are \n");
+		int k = 0;
+		do
+		{
+			ch = fgetc(ptr);
+			printf("%c", ch);
+			ans[k++] = ch;
+
+			// Checking if character is not EOF.
+			// If it is EOF stop reading.
+		} while (ch != EOF);
+		ans[k] = '\0';
+
+		// Closing the file
+		fclose(ptr);
+	}
+
+	sendto(sockfd, &ans, sizeof(ans), MSG_CONFIRM, (const struct sockaddr *)&cliaddr, len);
 	printf("Hello message sent.\n");
-		
+
 	return 0;
 }
